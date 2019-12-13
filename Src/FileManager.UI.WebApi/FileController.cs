b132e04ab024ad3daaa843ef9082afbbce65a341
於诗嘉 <yushijia@qq.com>
2019-12-13 16:09:16 +0800

    添加GetListFilesAsync

--- Src/FileManager.UI.WebApi/FileController.cs
+++ Src/FileManager.UI.WebApi/FileController.cs
@@ -58,6 +58,37 @@ namespace FileManagerCore.Controllers
                 return BadRequest(ex);
             }
         }
+        [HttpPost]
+        public async Task<ActionResult<byte[][]>> GetListFilesAsync(string path,string extension)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(path))
+                    throw new ArgumentNullException(nameof(path));
+
+                var DirectoryInfo = await _fileStore.GetDirectoryContentAsync(path, true);
+
+                if(DirectoryInfo == null)
+                    throw new ArgumentNullException(nameof(path));
+
+                var fileBytes = DirectoryInfo
+                    .Where(f => System.IO.Path.GetExtension(f.Path) == extension)
+                    .Select(ff =>
+                    {
+                        using (var r = _fileStore.GetFileStreamAsync(ff.Path).Result)
+                        {
+                            var stream = new System.IO.MemoryStream();
+                            r.CopyToAsync(stream).Wait();
+                            return stream.GetBuffer();
+                        }
+                    }).ToArray();
+                return fileBytes;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
         [HttpDelete]
         public async Task<ActionResult<bool>> DeleteFileAsync(string path)
         {
